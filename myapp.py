3bef419e90d1952683da0eee5f0d270f72ded4b9
Joshua <jmnorvor@st.ug.edu.gh>
2023-08-31 16:54:48 +0100

    another edit to myapp

--- myapp.py
+++ myapp.py
@@ -12,10 +12,13 @@ users_database = {
 def login():
     return render_template('login.html')
 
-@app.route('/')
-def next_page():
+@app.route('/validate/<username>/<password>')
+def validate(username, password):
+ if username in users_database and users_database[username] == password:
     return render_template('index.html')
 
+
+
     
-if __name__ == '__main__':
+ if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=4444)
